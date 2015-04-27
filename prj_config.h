a1eec9d5a1723db03a5a05f2b6df00e1ea9edac2
ufasoft <dev@ufasoft.com>
2015-04-27 13:38:31 +0500

    -

---
+++ prj_config.h
@@ -0,0 +1,14 @@
+#define VER_PRODUCTVERSION_MAJOR 0
+#define VER_PRODUCTVERSION_MINOR 113
+
+
+#ifndef VER_PRODUCTNAME_STR
+#	define VER_PRODUCTNAME_STR "Coin"
+#endif
+
+#ifndef VER_EXT_URL
+#	define VER_EXT_URL "http://ufasoft.com/coin"
+#endif
+
+#define VER_LEGALCOPYRIGHT_YEARS "2011-2015"
+
